60250bbad819b60c242f881b91c6238f6eb986d8
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-10 15:05:28 +0200

    update dashboard apiurl

--- public/api/apiuntis/units.js
+++ public/api/apiuntis/units.js
@@ -1,4 +1,4 @@
-import { db } from '../../firebase/firebaseAdmin';
+import { db } from '../../../web/firebase/firebaseAdmin';
 
 export default async function handler(req, res) {
   const { serverId } = req.query;
