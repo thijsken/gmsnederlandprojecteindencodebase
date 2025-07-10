8de4680dd39d0c9458e4e18dfb543f01c4ba95c7
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-10 23:20:01 +0200

    pages remove

--- public/api/apiluchtalarm/palen.js
+++ public/api/apiluchtalarm/palen.js
@@ -1,4 +1,4 @@
-const { db } = require("../../../web/firebase/firebaseAdmin");
+const { db } = require("../../../public/firebase/firebaseAdmin");
 
 export default async function handler(req, res) {
   const { serverId } = req.query;
