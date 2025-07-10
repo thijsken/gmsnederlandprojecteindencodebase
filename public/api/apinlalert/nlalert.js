86ad2cc4bcc936840d91af68d213c73c1a9f7c05
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-10 15:23:00 +0200

    update api nlalert

--- public/api/apinlalert/nlalert.js
+++ public/api/apinlalert/nlalert.js
@@ -1,4 +1,4 @@
-import { db } from "../../../web/firebase/firebaseAdmin";
+import { db } from "../../../public/firebase/firebaseAdmin";
 
 export default async function handler(req, res) {
   const { serverId } = req.query;
