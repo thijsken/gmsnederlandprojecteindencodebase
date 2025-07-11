2871970519ebb4b38cbc58f288aabcb0dc8a8dd5
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-11 12:08:48 +0200

    update als

--- web/server.js
+++ web/server.js
@@ -16,11 +16,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'web')));
 
 // Statische root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
 
 // API-routes
