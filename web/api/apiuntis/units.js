9da57cb5df06d7fc847066a9f8015a82791c1f83
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-14 13:35:25 +0200

    update

--- web/api/apiuntis/units.js
+++ web/api/apiuntis/units.js
@@ -1,4 +1,4 @@
-import { db } from '../../../web/firebase/firebaseAdmin';
+const { db } = require("../../../web/firebase/firebaseAdmin");
 
 export default async function handler(req, res) {
   const { serverId } = req.query;
