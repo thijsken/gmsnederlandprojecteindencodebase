2de71c4bd61e9b924b49ffce9134e2bbca2ef48d
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-14 13:51:36 +0200

    Update firebaseAdmin.js

--- web/firebase/firebaseAdmin.js
+++ web/firebase/firebaseAdmin.js
@@ -9,7 +9,7 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
-    databaseURL: 'https://gmsnederlandatabase-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL: process.env.FIREBASE_DATABASE_URL
   });
 
   console.log('✅ Firebase Admin initialized.');
