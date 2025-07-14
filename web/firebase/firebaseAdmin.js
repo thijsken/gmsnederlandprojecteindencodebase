e23e070cdbc29678ecfc4bad7f3ec566068e2839
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-14 15:12:14 +0200

    Update firebaseAdmin.js

--- web/firebase/firebaseAdmin.js
+++ web/firebase/firebaseAdmin.js
@@ -1,21 +1,21 @@
-import admin from "firebase-admin";
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    }),
-    databaseURL: "https://gmsnederlandatabase-default-rtdb.europe-west1.firebasedatabase.app"
-  });
+if (!privateKey) {
+  throw new Error("FIREBASE_PRIVATE_KEY environment variable is niet ingesteld.");
 }
-
-export default admin.database();
+console.log("FIREBASE_PRIVATE_KEY geladen:", !!process.env.FIREBASE_PRIVATE_KEY);
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: privateKey.replace(/\\n/g, '\n'),  // alleen uitvoeren als privateKey bestaat
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
+  databaseURL: "https://gmsnederlandatabase-default-rtdb.europe-west1.firebasedatabase.app"
+});
