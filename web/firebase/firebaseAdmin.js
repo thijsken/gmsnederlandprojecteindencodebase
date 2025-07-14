1103a6adff6b2a3d5e19ab27eeb41213b6e261de
thijsken <41241417+thijsken@users.noreply.github.com>
2025-07-14 13:59:09 +0200

    Update firebaseAdmin.js

--- web/firebase/firebaseAdmin.js
+++ web/firebase/firebaseAdmin.js
@@ -1,22 +1,25 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  console.log('🔐 Initializing Firebase Admin SDK...');
+  const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  };
 
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
-
-  console.log('✅ Firebase Admin initialized.');
-} else {
-  console.log('ℹ️ Firebase Admin already initialized.');
 }
 
 const db = admin.database();
 
-module.exports = db;
+module.exports = { db };
